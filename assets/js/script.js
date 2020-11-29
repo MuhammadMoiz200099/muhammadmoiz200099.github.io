ef7037e313c9910b6560a1a9465a8972bae2b243
Muhammad Moiz <muhammadmoiz0087@gmail.com>
2020-11-30 00:27:33 +0500

    init

---
+++ assets/js/script.js
@@ -0,0 +1,112 @@
+$(function () {
+
+    $('.navbar-toggle').click(function () {
+        $(this).toggleClass('act');
+        if ($(this).hasClass('act')) {
+            $('.main-menu').addClass('act');
+        }
+        else {
+            $('.main-menu').removeClass('act');
+        }
+    });
+
+    //jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(document).on('click', '.page-scroll a', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.site-header',
+        offset: 10
+    });
+
+    /* Progress bar */
+    var $section = $('.section-skills');
+    function loadDaBars() {
+        $('.progress .progress-bar').progressbar({
+            transition_delay: 500
+        });
+    }
+
+    $(document).bind('scroll', function (ev) {
+        var scrollOffset = $(document).scrollTop();
+        var containerOffset = $section.offset().top - window.innerHeight;
+        if (scrollOffset > containerOffset) {
+            loadDaBars();
+            // unbind event not to load scrolsl again
+            $(document).unbind('scroll');
+        }
+    });
+
+    /* Counters  */
+    if ($(".section-counters .start").length > 0) {
+        $(".section-counters .start").each(function () {
+            var stat_item = $(this),
+                offset = stat_item.offset().top;
+            $(window).scroll(function () {
+                if ($(window).scrollTop() > (offset - 1000) && !(stat_item.hasClass('counting'))) {
+                    stat_item.addClass('counting');
+                    stat_item.countTo();
+                }
+            });
+        });
+    };
+
+    // another custom callback for counting to infinity
+    $('#infinity').data('countToOptions', {
+        onComplete: function (value) {
+            count.call(this, {
+                from: value,
+                to: value + 1
+            });
+        }
+    });
+
+    $('#infinity').each(count);
+
+    function count(options) {
+        var $this = $(this);
+        options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+        $this.countTo(options);
+    }
+
+    // Navigation overlay
+    var s = skrollr.init({
+        forceHeight: false,
+        smoothScrolling: false,
+        mobileDeceleration: 0.004,
+        mobileCheck: function () {
+            //hack - forces mobile version to be off
+            return false;
+        }
+    });
+
+});
+
+function mailToMe() {
+    const name = $('#senderName').val();
+    const email = $('#senderEmail').val();
+    const message = $('#mesaage').val();
+
+    Email.send({
+        // SecureToken: '7fd52203-1785-4789-9fc6-42915e468571',
+        Host: "smtp.elasticemail.com",
+        Username: "muhammadmoizweb0087@gmail.com",
+        Password: "BC8953AABA1D311372BE4CE45C0A58FD0D18",
+        To: 'muhammadmoiz0087@gmail.com',
+        From: "muhammadmoizweb0087@gmail.com",
+        Subject: "Got a mail from " + name + " his email " + email,
+        Body: message,
+    })
+        .then(function (message) {
+            alert("Email sent successfully")
+            $('#senderName').val('');
+            $('#senderEmail').val('');
+            $('#mesaage').val('');
+        });
+}
